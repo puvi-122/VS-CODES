aad40171d494f41bc88d22aef8e419146fd020f6
puvi-122 <puviyarasanec.23eie@kongu.edu>
2025-06-20 16:05:25 +0530

    temperature

---
+++ day 4/Amazon/temperature.js
@@ -0,0 +1,13 @@
+const celsius1 = 25;
+const fahrenheit1 = (celsius1 * 9 / 5) + 32;
+console.log(`25°C is ${fahrenheit1}°F`); 
+
+
+const fahrenheit2 = 86;
+const celsius2 = (fahrenheit2 - 32) * 5 / 9;
+console.log(`86°F is ${celsius2}°C`); 
+
+
+const celsius3 = -5;
+const fahrenheit3 = (celsius3 * 9 / 5) + 32;
+console.log(`-5°C is ${fahrenheit3}°F`);
