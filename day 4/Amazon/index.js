efe07b4dffd746c265d23dd06a4da9bb0bf4e84a
puvi-122 <puviyarasanec.23eie@kongu.edu>
2025-06-20 16:12:20 +0530

    index

---
+++ day 4/Amazon/index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p id="msg">This is a paragraph</p>
+    <button class="js-button">Hi</button>
+    <script src="dom.js"></script>
+</body>
+</html>
