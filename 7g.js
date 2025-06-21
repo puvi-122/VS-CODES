f1809263ea37d4c5c0d4565be6a35c0000e3dd82
puvi-122 <puviyarasanec.23eie@kongu.edu>
2025-06-21 13:24:19 +0530

    7g

---
+++ 7g.js
@@ -0,0 +1,29 @@
+profile-image{
+    width: 50px;
+    height: 50px;
+    border-radius: 26px;
+    object-fit: cover;
+    object-position: center;
+    margin-right: 4px;
+    vertical-align: middle;
+}
+
+.tweet-input{
+    border: none;
+    width: 300px;
+    font-size: 18px;
+    vertical-align: middle;
+
+}
+
+
+.tweet-button{
+    background-color: blue;
+            color: azure;
+            padding: 8px 16px 8px 16px;
+            border-radius: 40px;
+            border: none;
+            font-weight: bold;
+            cursor: pointer;
+            vertical-align: middle;
+}
