eaae014cf5f447b486505815ec0cad9d0b815711
puvi-122 <puviyarasanec.23eie@kongu.edu>
2025-06-21 13:31:05 +0530

    9f

---
+++ 9f.js
@@ -0,0 +1,41 @@
+p{
+    margin: 0;
+    font-family: Arial;
+
+}
+
+.container{
+    width: 200px;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+}
+
+.profile-picture{
+    width: 200px;
+    height: 200px;
+    object-fit: cover;
+    margin-bottom: 12px;
+}
+
+.profile-name{
+    font-size: 10px;
+    font-weight: bold;
+    margin-left: 8px;
+    margin-bottom: 4px;
+}
+
+.mutual-friends{
+    color: rgb(100, 100, 100);
+    margin-left: 8px;
+    margin-bottom: 12px;
+}
+
+.add-friends-button{
+    background-color: rgb(32, 123, 243);
+    color: white;
+    border: none;
+    padding: 12px 20px;
+    border-radius: 4px;
+    font-size: 16px;
+    margin-left: 8px;
+    margin-bottom: 12px;
+}
