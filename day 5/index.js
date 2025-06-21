f958c4b1c2b8cd93a9b107e789ac782de2902ba0
puvi-122 <puviyarasanec.23eie@kongu.edu>
2025-06-21 10:53:39 +0530

    index.js

---
+++ day 5/index.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        button{
+            background-color: rgb(131, 173, 33);
+            padding: 6px 10px;
+            border: none;
+            color: antiquewhite;
+            transition: padding 1s;
+        }
+    button:hover{
+            padding: 16px 30px;
+    }
+    </style>
+</head>
+<body>
+    <button>Strech</button>
+</body>
+</html>
