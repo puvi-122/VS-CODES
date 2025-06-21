b1548c1a2c377914673f40415ac11d9688a300d3
puvi-122 <puviyarasanec.23eie@kongu.edu>
2025-06-21 13:20:05 +0530

    e5.js

---
+++ e5.js
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100..900&display=swap" rel="stylesheet">
+    <style>
+        p{
+            margin: 0;
+            font-family: "Montserrat";
+            text-align: center;
+        }
+        .new{
+            color: red;
+            margin-bottom: 8px;
+        }
+        .title{
+            font-size: 24px;
+        }
+        .tagline{
+            font-size: 40px;
+            font-weight: bold;
+            margin-bottom: 12px;
+        }
+        .price{
+            margin-bottom: 24px;
+        }
+        .buy-button{
+            background-color: blue;
+            color: azure;
+            padding: 8px 16px 8px 16px;
+            border-radius: 40px;
+            border: none;
+            font-weight: bold;
+            cursor: pointer;
+
+
+        }
+        .button-container{
+            display: flex;
+            justify-content: center;
+        }
+    </style>
+</head>
+<body>
+    <p class="new">new</p>
+    <p class="title">Mac Book</p>
+    <p class="tagline">supercharged abcded</p>
+    <p class="price">from $ 27898</p>
+    <div class="button-container">
+    <button class="buy-button">Buy</button>
+     </div>
+
+</body>
+</html>
