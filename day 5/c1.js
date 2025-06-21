54d67fbfb0acd158c150c0cbf570c67b17649256
puvi-122 <puviyarasanec.23eie@kongu.edu>
2025-06-21 10:59:39 +0530

    c1.js

---
+++ day 5/c1.js
@@ -0,0 +1,18 @@
+<style>
+    button {
+    background-color: black;
+    color: rgb(168, 17, 17);
+    border: none;
+    padding-top: 12px;
+    padding-bottom: 12px;
+    padding-left: 15px;
+    padding-right: 15px;
+    cursor: pointer;
+    transition: opacity 0.15s;
+  }
+
+  button:hover {
+    opacity: 0.8;
+  }
+</style>
+<button>Request now</button>
