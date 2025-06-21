af510768464134f3771ad348cf2dd8489f328fc8
puvi-122 <puviyarasanec.23eie@kongu.edu>
2025-06-21 10:58:05 +0530

    home.js

---
+++ day 5/home.js
@@ -0,0 +1,43 @@
+<style>
+  p {
+    font-family: Arial;
+    font-size: 18px;
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+
+  .tweet-heading {
+    margin-bottom: 4px;
+  }
+
+  .tweet-profile {
+    font-weight: bold;
+  }
+
+  .tweet-username {
+    color: rgb(100, 100, 100);
+  }
+
+  .tweet-content {
+    width: 600px;
+    line-height: 24px;
+    margin-bottom: 20px;
+  }
+
+  .twitter-handle {
+    color: rgb(0, 140, 255);
+  }
+</style>
+
+<p class="tweet-heading">
+  <span class="tweet-profile">freeCodeCamp.org</span>
+  <span class="tweet-username">@freeCodeCamp 1h</span>
+</p>
+
+<p class="tweet-content">
+  As a web developer, you'll want to make your projects easy to use and navigate around.
+</p>
+
+<p class="tweet-content">
+  Here <span class="twitter-handle">@chp_it</span> outlines the top skills new developers should have.
+</p>
